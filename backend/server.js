0cc823f4bbd42588c25648e92f2e55e089767729
Jay Desai <desaijay315@gmail.com>
2021-04-14 16:13:25 +0530

    user routes and controller added

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -21,7 +22,7 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 app.use('/api/products', productRoutes)
-app.use('/api/users', productRoutes)
+app.use('/api/users', userRoutes)
 
 
 app.use(notFound)
